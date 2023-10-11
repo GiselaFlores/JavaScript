464fa7795f7cd17874ad703a6f42c6682bb5c9e0
GiselaFlores <gi.flores@bue.edu.ar>
2023-10-11 16:21:34 -0300

    condicionales y bucles

--- index.js
+++ index.js
@@ -1,4 +1,78 @@
-let sephiroth = {
+let numero = parseInt(prompt("Ingrese su edad:"));
+/*
+if (numero < 18){
+    alert("no tenes permiso");
+} else {
+    
+    if(numero > 21){
+        alert("podes pasar");
+    } else {
+        alert("pedi permiso a tus padres");
+    }
+}
+
+if (numero < 18){
+    alert("no tenes permiso");
+} else if(numero > 21){
+        alert("podes pasar");
+    } else {
+        alert("pedi permiso a tus padres");
+    }
+
+
+if (numero < 18){
+        alert("no tenes permiso");
+    } else if(numero==18 || numero==19 ||(numero>=21)){
+        alert("podes pasar");
+    } else{
+        alert("si sos de 20 años no podes pasar");
+    }
+
+*/
+    let producto = parseInt(prompt("Ingrese codigo de producto:"));
+
+    switch(producto){
+        case 400:
+            alert("el producto es el codigo 100");
+            break;
+
+        case 200:
+            alert("el producto es el codigo 200");
+            break;
+
+        case 300:
+            alert("el producto es el codigo 300");
+            break;
+        
+        default:
+            alert("producto de codigo incorrecto");
+            break;
+    }
+
+
+// condicion ? "parte verdadera" : "parte falsa";
+
+let resultado = numero >=18 ? alert("Es mayor de edad") : "Es menor de edad";
+
+//Bucles FOR - WHILE - DO WHILE
+
+for (let i=0; i<11; i++){
+    console.log("El valor de i es: " + i);
+}
+
+while (producto == 100){
+    console.log("repeti producto");
+    producto = parseInt(prompt("Ingrese codigo de producto:"));
+}
+
+do {
+
+    console.log("repeti producto");
+    producto = parseInt(prompt("Ingrese codigo de producto:"));
+} while (producto == 100);
+
+
+/*let sephiroth = {
     id: 1,
     name: "Sephiroth",
     lastName: "Crescent",
@@ -6,7 +80,11 @@ let sephiroth = {
     loveInterest: "Aerith Gainsborough",
     family: {
         mothers:"Lucrezia Crescent",
-        father: "Professor Hojo"
+        father: "Professor Hojo",
+        hermanos: {
+            hermano1: "kjhkjhkjhkjh",
+            hermano2: "5jbjhvhjvjh"
+        }
     },
     job: "Soldier First Class Shinra",
     alias: "One Winged Angel",
@@ -16,12 +94,13 @@ let sephiroth = {
 
 console.log({sephiroth});
 
-console.log("Su padre es: " + sephiroth.family.father + " " +"Su madre es: " + sephiroth.family.mothers);
+console.log(sephiroth.family.hermanos.hermano2);
 
-//alert("Su padre es: " + sephiroth.family.father + " " +"Su madre es: " + sephiroth.family.mothers)
+console.log("Su padre es: " + sephiroth.family.father +"Su madre es: " + sephiroth.family.mothers);
 
 //Operadores aritmeticos
-let numero8 = 50, numero9 = 2;
+
+let numero8= "50", numero9 = 50;
 
 let suma = numero8 + numero9;
 console.log(suma);
@@ -39,83 +118,62 @@ let resto = numero8 % numero9;
 console.log(resto);
 
 //Operadores de asignacion
-//let asignoMas = numero8 + 10;
 
-let asignoMas = numero8;
+let asigMas = numero8;
 
-//asignoMas = asignoMas + 10;
-asignoMas += 10;
-console.log(asignoMas);
+//asigMas = asigMas +10;
+asigMas += 10;
+console.log(asigMas);
 
-asignoMas -= 10;
-console.log(asignoMas);
+asigMas -= 10;
+console.log(asigMas);
 
-asignoMas *= 10;
-console.log(asignoMas);
+asigMas *= 10;
+console.log(asigMas);
 
-asignoMas /= 10;
-console.log(asignoMas);
+asigMas /= 10;
+console.log(asigMas);
 
-asignoMas %= 10;
-console.log(asignoMas);
+asigMas %= 10;
+console.log(asigMas);
 
-//Operadores comparacion
+//operadores de comparacion
 
-let mayorQue = numero8 > numero9;
+let mayorQue = numero8 !== numero9;
 console.log(mayorQue);
 
-let menorQue = numero8 < numero9;
-console.log(menorQue);
+//operadores de incremento o decremento
 
-let mayorIgualQue = numero8 >= numero9;
-console.log(mayorIgualQue);
-
-let menorIgualQue = numero8 <= numero9;
-console.log(menorIgualQue);
-
-let igualQue = "10" == 10;
-console.log(igualQue);
-
-let diferenteQue = "10" != 10;
-console.log(diferenteQue);
-
-let igualesQue = "10" === 10;
-console.log(igualesQue);
-
-let diferenteesQue = "10" !== 10;
-console.log(diferenteesQue);
-
-//operadores de incremento y decremento
-
-let numero10 = 20;
+let numero10 =20;
 
 numero10++;
 ++numero10;
 
-console(numero10);
+console.log(numero10);
 
 numero10--;
 --numero10;
 
-console(numero10);
+console.log(numero10);
 
 
-//operadores logicas
+//operadores logicos
 
-// (&&) se cumpla ambos criterios
+// (&&) cumple ambos criterios
 
-let resultado = (numero8 > 3) && (numero8 <= 10); 
+let resultado = (numero8 >= 3) && (numero8 <= 10) && (numero8 !=5);
+console.log(resultado);
 
-// (||)   se cumpla uno solo pero no todos
-
-let resultado2 = (numero9 > 3) || (numero8 <= 10); 
+// (||) al menos 1 se cumpla
+let resultado2 = (numero8 >= 3) || (numero8 <= 10) || (numero8 !=5);
+console.log(resultado2);
 
 // negacion
 
 !resultado;
 
 
-/* console.log("hola");
+ console.log("hola");
 
 let nombreVariable = "Hola chicos";
 console.log(nombreVariable);
